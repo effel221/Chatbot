6d71b2831325f83826b889f024b38e76326bb126
Tatsiana Salikayeva <t.salikayeva@gmail.com>
2021-01-22 23:53:49 +0000

    added rx library

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { interval, merge } from "rxjs";
+import { map } from "rxjs/operators";
 
 import './reset.scss'
 import './App.scss'
+
+const createTweetSource = (frequency:number, account:String, attribute:String) => {
+  return interval(frequency).pipe(map(i => ({
+    account,
+    timestamp: Date.now(),content: `${attribute} Tweet number ${i + 1}`
+  })));
+}
+const tweets = merge(
+    createTweetSource(5000, 'AwardsDarwin', 'Facepalm'),
+    createTweetSource(3000, 'iamdevloper', 'Expert'),
+    createTweetSource(5000, 'CommitStrip', 'Funny')
+);
+tweets.subscribe(console.log.bind(console));
 
 const App = () => {
   return (
