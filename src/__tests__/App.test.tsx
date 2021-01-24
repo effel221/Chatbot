4b4fe7d2597bae4a5da4addd155243496d1dd97f
Tatsiana Salikayeva <t.salikayeva@gmail.com>
2021-01-24 16:08:22 +0000

    Added tests and removed items after 30sec

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,56 @@
-
-import { render, fireEvent, waitForElement } from "@testing-library/react";
-
+import React from "react";
 import App  from "../Components/App";
+import Loader  from "../Components/Loader";
+import TweetItems  from "../Components/TweetItems";
+
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
 
 describe("<App />", () => {
-  test("should display a blank login form, with remember me checked by default", async () => {
-    // ???
+  test('Component App should be rendered', () => {
+    render(<App />)
+  })
+});
+
+describe("<Loader />", () => {
+  test('Component Loader should be rendered', () => {
+    render(<Loader />)
+  })
+});
+
+describe("<TweetItems />", () => {
+  const tweetsArrBase = {
+    account: 'AwardsDarwin',
+    timestamp: 8753958028903,
+    content: 'Facepalm',
+  };
+  const notLikedTweetItem = [{...tweetsArrBase, liked: false}];
+  const likedTweetItem = [{...tweetsArrBase, liked: true}];
+  const setTweetsArr = jest.fn();
+  const setLikedItemsAmount = jest.fn();
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('Component TweetItems should be rendered', () => {
+    const TweetItemsComp = render(
+    <TweetItems
+        tweetsArr={notLikedTweetItem}
+        likedState={false}
+        setTweetsArr={setTweetsArr}
+        setLikedItemsAmount={setLikedItemsAmount}
+    />);
+  });
+  test('Component TweetItem should be liked', () => {
+    const TweetItemsCompLiked = render(<TweetItems
+        tweetsArr={likedTweetItem}
+        likedState={true}
+        setTweetsArr={setTweetsArr}
+        setLikedItemsAmount={setLikedItemsAmount}
+    />);
+    const likedElemCount = TweetItemsCompLiked.container.getElementsByClassName('liked').length;
+    expect(likedElemCount).toBe(2);
   });
 });
